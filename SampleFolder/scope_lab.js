3ff6e08913db39ff1ef03dfadc3275c538d2c9e5
gregj76 <133953724+gregj76@users.noreply.github.com>
2025-12-28 21:32:59 +0100

    adding final project

--- SampleFolder/scope_lab.js
+++ SampleFolder/scope_lab.js
@@ -2,6 +2,12 @@
 var globalVar = "I'm a global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
+
+for (let i = 1; i <= 5; i++) {
+ console.log(i);
+}
+
+
 {
 // Block scope
 var blockVar = "I'm a block-scoped var";
@@ -15,8 +21,60 @@ function show(){
     var functionVar = "I'm a block-scoped var";
     let functionLet = "I'm a block-scoped let";
     const functionConst = "I'm a block-scoped const";
+    
+    console.log(functionVar);
+    console.log(functionLet);
+    console.log(functionConst);
     }
-    show();
-    console.log(functionVar); // Throws ReferenceError
-    console.log(functionLet); // Throws ReferenceError
-    console.log(functionConst); // Throws ReferenceError
+
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+1
+console.log("User Category:", userCategory);
+
+(function immediateFunction() {
+    var immediateVar = "I'm inside an IIFE";
+    console.log(immediateVar);
+})();
+
+//console.log(immediateVar); // This will cause an error because immediateVar is not defined outside the IIFE
+
+const button= document.getElementById("myButton");
+
+const outputElement = document.getElementById("output");
+
+button.onclick = function() {
+    outputElement.textContent = `
+    Global Var: ${globalVar}
+    Global Let: ${globalLet}
+    Global Const: ${globalConst}
+    Block Var: ${blockVar}`; };
